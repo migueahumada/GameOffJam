5ab5285941e9b121feeda284ed5a101845afd660
Hasi-b <hasib.hasan9@gmail.com>
2025-11-30 23:45:53 +0200

    settings logic

--- Assets/Hasib/UIScript/SetttingsManager.cs
+++ Assets/Hasib/UIScript/SetttingsManager.cs
@@ -1,8 +1,27 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetttingsManager : MonoBehaviour
 {
     [SerializeField] GameObject settingsPanel;
+    [SerializeField] private Slider Music;
+    [SerializeField] private Slider SFX;
+    [SerializeField] private float defaultValue = 0.75f;
+
+    [SerializeField]
+    float currentSFXValue;
+    [SerializeField]
+    float currentMusicValue;
+    private void Awake()
+    {
+        Music.value = defaultValue;
+        SFX.value = defaultValue;
+    }
+
+
+   
+
     public void ShowSettings()
     {
         settingsPanel.SetActive(true);
@@ -11,5 +30,17 @@ public class SetttingsManager : MonoBehaviour
     public void HideSettings()
     {
         settingsPanel.SetActive(false);
+    }
+
+    public void SetSFXValue()
+    {
+        currentSFXValue = SFX.value;
+        Debug.Log("CSF: "+currentSFXValue);
+    }
+
+    public void SetMusicValue()
+    {
+        currentMusicValue =Music.value;
+        Debug.Log("CM: "+currentMusicValue);
     }
 }
